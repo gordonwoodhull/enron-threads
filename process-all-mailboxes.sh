fc7b49148ec0a7f03472ea0fea181eb98abaeac0
Gordon Woodhull <gordon@woodhull.com>
2018-02-21 14:39:41 -0500

    follow simlinks

--- process-all-mailboxes.sh
+++ process-all-mailboxes.sh
@@ -5,7 +5,7 @@ PREFIX=$3
 COUNT=0
 for d in `ls -1d $DIR/* | gshuf # | head -1`; do
     NAME=`echo $d | sed "s:$1/::"`
-    (TIME=`time (find $d -type f | (while read f; do $SDIR/process-file.sh $f; done) | $CMD > $PREFIX-$NAME.txt)`; echo $COUNT $NAME; echo $TIME) &
+    (TIME=`time (find -L $d -type f | (while read f; do $SDIR/process-file.sh $f; done) | $CMD > $PREFIX-$NAME.txt)`; echo $COUNT $NAME; echo $TIME) &
     ((COUNT++))
 done
 
