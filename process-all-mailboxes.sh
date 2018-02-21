8ab717b7b5e54ced20325ce8f6323fa70d11af33
Gordon Woodhull <gordon@woodhull.com>
2018-02-21 11:03:25 -0500

    parallelize mailbox processing

--- process-all-mailboxes.sh
+++ process-all-mailboxes.sh
@@ -1,2 +1,11 @@
 SDIR=`echo $0 | sed "s:/[^/]*$::"`
-for d in `ls -1d $1/* | gshuf # | head -1`; do find $d -type f | (while read f; do $SDIR/process-file.sh $f; done); done
+DIR=$1
+CMD=$2
+PREFIX=$3
+COUNT=0
+for d in `ls -1d $DIR/* | gshuf # | head -1`; do
+    NAME=`echo $d | sed "s:$1/::"`
+    (TIME=`time (find $d -type f | (while read f; do $SDIR/process-file.sh $f; done) | $CMD > $PREFIX-$NAME.txt)`; echo $COUNT $NAME; echo $TIME) &
+    ((COUNT++))
+done
+
