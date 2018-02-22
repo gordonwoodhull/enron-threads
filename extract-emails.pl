6d54cb8e0f377131938c2e2455fd4f618f84a011
Gordon Woodhull <gordon@woodhull.com>
2018-02-22 06:52:38 -0500

    attempt to separate original, name, address
    but still passing some crlf through yuk

--- extract-emails.pl
+++ extract-emails.pl
@@ -6,7 +6,11 @@ use Email::Address;
 while (my $line = <>) {
     my @emails = Email::Address->parse($line);
     if (@emails) {
-        print $_, "\n" for @emails;
+        for my $addr (@emails) {
+            my $orig = $addr->original;
+            $orig =~ s/\r\n\z//g;
+            print $orig, "\t", $addr->name, "\t", $addr->address, "\n"
+        }
     }
 }
 
