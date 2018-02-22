be3692f3f8cac822266a4b7976a37fc214eefbc1
Gordon Woodhull <gordon@woodhull.com>
2018-02-22 06:52:08 -0500

    better way to take 1 for testing

--- process-all-mailboxes.sh
+++ process-all-mailboxes.sh
@@ -3,7 +3,7 @@ DIR=$1
 CMD=$2
 PREFIX=$3
 COUNT=0
-for d in `ls -1d $DIR/* | gshuf # | head -1`; do
+for d in `ls -1d $DIR/* | gshuf # -n 1`; do
     NAME=`echo $d | sed "s:$1/::"`
     (TIME=`time (find -L $d -type f | (while read f; do $SDIR/process-file.sh $f; done) | $CMD > $PREFIX-$NAME.txt)`; echo $COUNT $NAME; echo $TIME) &
     ((COUNT++))
