3ac6990449d3bac52b090bf38e7f3bd2d79561b6
Gordon Woodhull <gordon@woodhull.com>
2018-03-07 22:40:16 -0500

    move command to end and pass all args thru

--- process-all-mailboxes.sh
+++ process-all-mailboxes.sh
@@ -6,13 +6,13 @@ while [ "$1" != "" ]; do
         -n) LIST=$2; shift ;;
         --help) cat <<EOF
 
- Usage: $0 [--1] dir command prefix
+ Usage: $0 [--1] dir prefix command...
 
  Run a command in parallel on all files found within some subdirectories
 
  'dir' is the path to look for subdirectories. fork this script in each subdirectory, then
- 'command' is the script to run on each file
  'prefix' is the start of the output filename, $PREFIX-$NAME.txt
+ 'command...' is the script with arguments to pipe the files through
 
  -1 only one random subdirectory
  -n name choose only the named subdirectory
@@ -26,17 +26,20 @@ done
 
 SDIR=`echo $0 | sed "s:/[^/]*$::"`
 DIR=$1
-CMD=$2
-PREFIX=$3
+PREFIX=$2
+shift;shift;
+CMD=$*
+
 COUNT=0
 case $LIST in
     star) LIST="ls -1d $DIR/*" ;;
     *) LIST="echo $DIR/$LIST" ;;
 esac
 echo "$LIST | $SHUF"
+echo $CMD
 for d in `$LIST | $SHUF`; do
     echo Starting $COUNT $d..
-    NAME=`echo $d | sed "s:$1/::"`
+    NAME=`echo $d | sed "s:$DIR/::"`
     (TIME=`(time (find -L $d -type f | (while read f; do $SDIR/process-file.sh $f; done) | $CMD > $PREFIX-$NAME.txt)) 2>&1`; echo Finished $COUNT $NAME; echo $TIME) &
     ((COUNT++))
 done
