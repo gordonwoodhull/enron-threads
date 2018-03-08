2a089d96ce8584cf68911d3b2f779f8eb887c043
Gordon Woodhull <gordon@woodhull.com>
2018-03-07 22:39:08 -0500

    just keep name and address
    original text is too messy

--- extract-emails.pl
+++ extract-emails.pl
@@ -9,7 +9,7 @@ while (my $line = <>) {
         for my $addr (@emails) {
             my $orig = $addr->original;
             $orig =~ s/\r\n\z//g;
-            print $orig, "\t", $addr->name, "\t", $addr->address, "\n"
+            print $addr->name, "\t", $addr->address, "\n"
         }
     }
 }
