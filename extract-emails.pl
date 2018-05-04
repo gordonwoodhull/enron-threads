cc7e01025fcb5af98ce4fd900c998931863970d6
Gordon Woodhull <gordon@woodhull.com>
2018-05-04 14:34:40 -0400

    canonicalize name

--- extract-emails.pl
+++ extract-emails.pl
@@ -10,7 +10,18 @@ while (my $line = <>) {
             my $orig = $addr->original;
             $orig =~ s/\r\n\z//g;
             if ($addr->address !~ m/@[^.]*$/) {
-                (my $name = $addr->name) =~ s/^['"]*//; $name =~ s/['"]*$//;
+                # canonical version of name: first lowercase
+                my $name = lc $addr->name;
+                # remove begin/end quotes from names
+                $name =~ s/^['"]*//;
+                $name =~ s/['"]*$//;
+                # convert dots to spaces
+                $name =~ s/\./ /g;
+                # sometimes emails slip through - drop @ part
+                $name =~ s/@.*$//;
+                # last, first => first last
+                $name = join(' ', reverse split(/, */, $name)) if $name =~ m/,/;
+                # just remove begin/end quotes from emails
                 (my $address = $addr->address) =~ s/^['"]*//; $address =~ s/['"]*$//;
                 print $name, "\t", $address, "\n"
             }
