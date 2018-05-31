fdd37fae0e05aa0ce2266fbc403360c34747015a
Gordon Woodhull <gordon@woodhull.com>
2018-05-31 17:41:40 -0400

    show usage when not enough args

--- process-all-mailboxes.sh
+++ process-all-mailboxes.sh
@@ -2,31 +2,37 @@ set -e
 SHUF=gshuf
 LIST=star
 SLOW=0
+
+function usage {
+ cat <<EOF
+
+ Usage: $0 [-1] [-n name] [-slow N] dir prefix ext command...
+
+ Run a command in parallel on all files found within some subdirectories
+
+ 'dir' is the path to look for subdirectories. fork this script in each subdirectory, then
+ 'prefix' is the start of the output filename, \$PREFIX-\$NAME.\$EXT
+ 'ext' is the filename extension
+ 'command...' is the script with arguments to pipe the files through
+
+ -1		only one random subdirectory
+ -n name	choose only the named subdirectory
+ -slow N	pause N seconds after spawning each task (to conserve resources)
+
+EOF
+}
 while [ "$1" != "" ]; do
     case $1 in
         -1) SHUF="$SHUF -n 1" ;;
         -n) LIST=$2; shift ;;
         -slow) SLOW=$2; shift ;;
-        --help) cat <<EOF
-
- Usage: $0 [-1] dir prefix ext command...
-
- Run a command in parallel on all files found within some subdirectories
-
- 'dir' is the path to look for subdirectories. fork this script in each subdirectory, then
- 'prefix' is the start of the output filename, $PREFIX-$NAME.$EXT
- 'ext' is the filename extension
- 'command...' is the script with arguments to pipe the files through
-
- -1 only one random subdirectory
- -n name choose only the named subdirectory
-
-EOF
-        exit 0 ;;
+        --help) usage; exit 0 ;;
         *) break 2 ;;
     esac
     shift
 done
+
+if [ $# -lt 4 ]; then usage; fi;
 
 SDIR=`echo $0 | sed "s:/[^/]*$::"`
 DIR=`echo $1 | sed "s:/$::"`
